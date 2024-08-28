89cbb2ba6d1704878d0588b7e903b9874c744d79
Eduard Iakovlev <yakovlevadn@mail.ru>
2024-08-28 13:48:27 +0700

    убрал линее

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         CalcWork calc = new CalcWork();
         calc.calcStart();
     }
